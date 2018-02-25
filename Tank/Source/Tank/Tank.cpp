a259dd84c414b295eba5c120258ba32a42b5e4eb
Mr.Wu <12259145@163.com>
2018-02-26 01:15:27 +0800

    0.0.1
    init project

---
+++ Tank/Source/Tank/Tank.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tank.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tank, "Tank" );
